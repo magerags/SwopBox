bd802a939ee104f1f712ac0dd3928156bfc25e92
Tom Whittle <tomwhittle@hey.com>
2021-03-10 14:51:02 +0100

    updated routes to include box and booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :visitors, :only => [:new, :create]
+  resource :boxes
+  resource :bookings
   root to: 'visitors#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
