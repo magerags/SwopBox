4ce4728ab37c846b30731f6c24532bfb0b5da5ee
Tom Whittle <tomwhittle@hey.com>
2021-03-10 14:55:02 +0100

    fixed routes plural

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :visitors, :only => [:new, :create]
-  resource :boxes
-  resource :bookings
+  resources :visitors, :only => [:new, :create]
+  resources :boxes
+  resources :bookings
   root to: 'visitors#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
