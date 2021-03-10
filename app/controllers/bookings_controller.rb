021584fc5dacca577c5d54808cb42f2208eca230
Tom Whittle <tomwhittle@hey.com>
2021-03-10 14:45:22 +0100

    added box and booking controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,48 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @booking = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+  end 
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to booking_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to booking_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:name, :description, :medium, :size, :collection_id, :photo, :featured)
+  end
+
+  def set_booking
+    @booking = booking.find(params[:id])
+  end
 end
--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,2 +1,48 @@
 class BoxesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_box, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @boxes = Box.all
+  end
+
+  def show
+  end
+
+  def new
+    @box = Box.new
+  end
+
+  def edit
+  end 
+
+  def update
+    @box.update(box_params)
+    redirect_to boxes_path
+  end
+
+  def create
+    @box = Box.new(box_params)
+
+    if @box.save
+      redirect_to boxes_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @box.destroy
+    redirect_to boxes_path
+  end
+
+  private
+
+  def box_params
+    params.require(:box).permit(:name, :description, :medium, :size, :collection_id, :photo, :featured)
+  end
+
+  def set_box
+    @box = Box.find(params[:id])
+  end
 end
