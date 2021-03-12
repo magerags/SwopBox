f8bb154e207ce008ae50c5ecf9d65669164b9f92
Tom Whittle <tomwhittle@hey.com>
2021-03-12 09:37:34 +0100

    added full name to users and corrected model relationships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :company)}
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :company, :full_name)}
 
             devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :company)}
        end
